e19a4ee7b660a3e322605e373efed70b9f2fc25c
Derek Anderson <derek.anderson@lge.com>
2015-04-21 13:28:21 -0700

    enyo samples added

---
+++ index.js
@@ -0,0 +1,42 @@
+var 
+	kind = require('enyo/kind'),
+	ready = require('enyo/ready');
+
+var
+	Anchor = require('enyo/Anchor'),
+	Collection = require('enyo/Collection'),
+	Control = require('enyo/Control'),
+	DataRepeater = require('enyo/DataRepeater');
+
+var
+	samples = {
+		Enyo: require('enyo-samples')
+	};
+
+var 
+	List = kind({
+		components: [
+			{name: 'list', kind: DataRepeater, components: [
+				{style: 'margin: 10px;', components: [
+					{name: 'a', kind: Anchor}
+				], bindings: [
+					{from: 'model.name', to: '$.a.href', transform: function (v) { return '?' + v; }},
+					{from: 'model.name', to: '$.a.content', transform: function (v) { return v + ' Samples'; }}
+				]}
+			]}
+		],
+		create: function () {
+			Control.prototype.create.apply(this, arguments);
+			this.$.list.set('collection', new Collection(Object.keys(samples).map(function (key) {
+				return {name: key};
+			})));
+		}
+	});
+
+ready(function(){
+	var names = window.document.location.search.substring(1).split('&');
+	var name = names[0];
+	var sample = samples[name] || List;
+
+	new sample().renderInto(document.body);
+});
