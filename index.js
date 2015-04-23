cf3ee557b5dceae820763f2f27419b6117defa90
Derek Anderson <derek.anderson@lge.com>
2015-04-23 14:23:08 -0700

    canvas

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var
 		Onyx: require('onyx-samples'),
 //		Moonstone: require('moonstone-samples'), //router blocking
 		Spotlight: require('spotlight-samples'),
-	  	iLib: require('enyo-ilib-samples') //doesn't build
-		
+	  	iLib: require('enyo-ilib-samples'), //doesn't build
+	  	Canvas: require('canvas-samples') //doesn't build
 	};
 
 var 
