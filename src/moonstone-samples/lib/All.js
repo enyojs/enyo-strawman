fab0aabf90ad981c3914334ef8ca57383230058b
Aaron Tam <aaron.tam@lge.com>
2015-05-15 11:50:12 -0700

    prevent menu button from being spotted while closed.
    Issue: ENYO-1650
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- src/moonstone-samples/lib/All.js
+++ src/moonstone-samples/lib/All.js
@@ -110,12 +110,12 @@ module.exports = kind({
 		}
 	},
 	components: [
-		{classes: 'moon-sample-persistant-hotspot', components: [
-			{classes: 'moon-sample-persistant-frame', spotlight: 'container', components: [
-				{kind: Button, content: 'Reload', small: true, spotlight: false, classes: 'moon-sample-persistant-locale-button', ontap: 'reload'},
-				{kind: Button, content: 'Back to List', small: true, spotlight: false, classes: 'moon-sample-persistant-locale-button', ontap: 'backToList'},
+		{classes: 'moon-sample-persistent-hotspot', components: [
+			{classes: 'moon-sample-persistent-frame', spotlight: 'container', components: [
+				{kind: Button, content: 'Reload', small: true, spotlight: false, classes: 'moon-sample-persistent-locale-button', ontap: 'reload'},
+				{kind: Button, content: 'Back to List', small: true, spotlight: false, classes: 'moon-sample-persistent-locale-button', ontap: 'backToList'},
 				{kind: ContextualPopupDecorator, components: [
-					{kind: Button, content: 'Set Locale', small: true, spotlight: false, classes: 'moon-sample-persistant-locale-button'},
+					{kind: Button, content: 'Set Locale', small: true, spotlight: false, classes: 'moon-sample-persistent-locale-button'},
 					{name: 'localePopup', kind: ContextualPopup, classes: 'moon-sample-locale-popup', components: [
 						{content: 'Set Locale', kind: Divider},
 						{name: 'localeRepeater', kind: DataRepeater, ontap: 'localeListTapped', selection: true, groupSelection: true, selectionProperty: 'selected', containerOptions: {kind: Scroller, classes: 'enyo-fill'}, fit: true, components: [
@@ -127,7 +127,7 @@ module.exports = kind({
 						]}
 					]}
 				]},
-				{kind: ToggleButton, toggleOffLabel: 'Dark Theme', toggleOnLabel: 'Light Theme', small: true, ontap: 'handleThemeTap'}
+				{kind: ToggleButton, toggleOffLabel: 'Dark Theme', toggleOnLabel: 'Light Theme', small: true, spotlight: false, ontap: 'handleThemeTap'}
 			]}
 		]},
 		{name: 'home'},
