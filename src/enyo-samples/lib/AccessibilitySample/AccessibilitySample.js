b89e6e425ed11eedac64658ff053f6310a6bd338
jaewon98.jang <jaewon98.jang@lgepartner.com>
2015-06-04 11:34:02 +0900

    add accessibility samples for accessibilityDisabled
    add accessibilityDisabled sample code in AccessibilitySample.js
    add accessibilityDisabled example for UI control (checkbox, button).
    add popup accessibilityDisabled example.
    change code for checkbox, button accessibilityDisabled example.
    
    https://jira2.lgsvl.com/browse/ENYO-1689
    Enyo-DCO-1.1-Signed-off-by: Jaewon Jang <jaewon98.jang@lgepartner.com>

--- src/enyo-samples/lib/AccessibilitySample/AccessibilitySample.js
+++ src/enyo-samples/lib/AccessibilitySample/AccessibilitySample.js
@@ -2,7 +2,13 @@ var
 	kind = require('enyo/kind'),
 	AccessibilitySupport = require('enyo/AccessibilitySupport'),
 	Button = require('enyo/Button'),
-	Control = require('enyo/Control');
+	ButtonAccessibilitySupport = require('enyo/Button/ButtonAccessibilitySupport.js'),
+	Checkbox = require('enyo/Checkbox'),
+	CheckboxAccessibilitySupport = require('enyo/Checkbox/CheckboxAccessibilitySupport.js'),
+	Control = require('enyo/Control'),
+	InputAccessibilitySupport = require('enyo/Input/InputAccessibilitySupport.js'),
+	Popup = require('enyo/Popup');
+	PopupAccessibilitySupport = require('enyo/Popup/PopupAccessibilitySupport.js'),
 
 module.exports = kind({
 	components: [
@@ -29,8 +35,36 @@ module.exports = kind({
 		]},
 		{kind: Button, content: 'Toggle Content', ontap: 'toggleContent'},
 		{kind: Button, content: 'Toggle Label', ontap: 'toggleLabel'},
-		{kind: Button, content: 'Toggle Hint', ontap: 'toggleHint'}
+		{kind: Button, content: 'Toggle Hint', ontap: 'toggleHint'},
+		{name: "toggle1", kind: Button, content: 'Toggle Disabled', ontap: 'toggleDisabled'},
+		{tag: 'h3', content: 'UI control accessibilityDisabled Example'},
+		{ components: [
+			{kind: Popup, name: "popup", autoDismiss: false, classes: "popup", content: "popup", mixins: [AccessibilitySupport, PopupAccessibilitySupport]},
+			{tag: 'br'},
+			{tag: 'br'},
+			{name: 'popup_result'},
+			{tag: 'br'},
+			{name: 'checkbox', kind: Checkbox, disabled: true, checked: true, content: 'simple checkbox', mixins: [AccessibilitySupport, InputAccessibilitySupport, CheckboxAccessibilitySupport]},
+			{tag: 'br'},
+			{name: 'checkbox_result'},
+			{tag: 'br'},
+			{name: 'btn', kind: Button, disabled: true, content: 'button', mixins: [AccessibilitySupport, ButtonAccessibilitySupport]},
+			{tag: 'br'},
+			{name: 'btn_result'},
+			{tag: 'br'}
+		]},
+		{name: "toggle2", kind: Button, content: 'Toggle Disabled', ontap: 'toggleDisabled'}
 	],
+	/**
+	* @private
+	*/
+	rendered: kind.inherit(function (sup) {
+		return function (props) {
+			sup.apply(this, arguments);
+			this.$.popup.setShowing(true);
+			this.showAriaAttributes();
+		};
+	}),
 	toggleContent: function () {
 		this.$.dyn.set('content', this.$.dyn.content ? '' : 'Content');
 	},
@@ -39,5 +73,36 @@ module.exports = kind({
 	},
 	toggleHint: function () {
 		this.$.dyn.set('accessibilityHint', this.$.dyn.accessibilityHint ? '' : 'Hint');
+	},
+	toggleDisabled: function (sender, e) {
+		if (sender.name === 'toggle1') {
+			this.$.dyn.set('accessibilityDisabled', this.$.dyn.accessibilityDisabled ? false : true);
+		} else if (sender.name === 'toggle2') {
+			this.$.popup.set('accessibilityDisabled', this.$.popup.accessibilityDisabled ? false : true);
+			this.$.checkbox.set('accessibilityDisabled', this.$.checkbox.accessibilityDisabled ? false : true);
+			this.$.btn.set('accessibilityDisabled', this.$.btn.accessibilityDisabled ? false : true);
+			this.showAriaAttributes();
+		}
+	},
+	showAriaAttributes: function () {
+		var role, label, checked, disabled, tabindex;
+
+		label = this.$.popup.getAttribute('aria-label');
+		role = this.$.popup.getAttribute('role');
+		tabindex = this.$.popup.getAttribute('tabindex');
+		this.$.popup_result.setContent(' :: tabindex = ' + tabindex + ' :: role = ' + role + ' :: aria-label = ' + label);
+
+		label = this.$.checkbox.getAttribute('aria-label');
+		role = this.$.checkbox.getAttribute('role');
+		checked = this.$.checkbox.getAttribute('aria-checked');
+		disabled = this.$.checkbox.getAttribute('aria-disabled');
+		tabindex = this.$.checkbox.getAttribute('tabindex');
+		this.$.checkbox_result.setContent(' :: tabindex = ' + tabindex + ' :: role = ' + role + ' :: aria-label = ' + label + ' :: aria-checked = ' + checked + ' :: aria-disabled = ' + disabled);
+	
+		label = this.$.btn.getAttribute('aria-label');
+		role = this.$.btn.getAttribute('role');
+		disabled = this.$.btn.getAttribute('aria-disabled');
+		tabindex = this.$.btn.getAttribute('tabindex');
+		this.$.btn_result.setContent(' :: tabindex = ' + tabindex + ' :: role = ' + role + ' :: aria-label = ' + label + ' :: aria-disabled = ' + disabled);
 	}
 });
