c1aa4e05af4dfca21722d3d936e66e4ef5883208
Suhyung Lee <loveew99@gmail.com>
2015-06-26 11:26:52 +0900

    Update ProgressSample.js
    changed property to meet Ryan's change on progressBar

--- src/moonstone-samples/lib/ProgressSample.js
+++ src/moonstone-samples/lib/ProgressSample.js
@@ -19,7 +19,7 @@ module.exports = kind({
 		{kind: ProgressBar, progress: 50, barClasses: 'moon-sample-green'},
 		{kind: ProgressBar, progress: 50, barClasses: 'moon-sample-red'},
 		{kind: Divider, content: 'Progress Bars with Popup'},
-		{kind: ProgressBar, progress: 25, enablePopup: true},
+		{kind: ProgressBar, progress: 25, noPopup: false},
 		{tag: 'br'},
 		{kind: InputDecorator, style: 'margin-right:10px;', components: [
 			{kind: Input, placeholder: 'Value'}
