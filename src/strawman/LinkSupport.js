af3f73254dd06cc7bff0951795ed9b5a0ed455b8
Blake Stephens <blake.stephens@lge.com>
2015-07-31 11:31:59 -0700

    Simplified the tap handler to use the existing tap function reference.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- src/strawman/LinkSupport.js
+++ src/strawman/LinkSupport.js
@@ -6,10 +6,8 @@
 
 module.exports = {
 	name: 'LinkSupport',
-	handlers: {
-		ontap: 'handleLink'
-	},
-	handleLink: function (sender, ev) {
+	tap: function (sender, ev) {
+		this.inherited(arguments);
 		var link = this.get('href') || ev.originator.get('href') || ev.originator.parent.get('href');
 		if (link) {
 			window.location.href = link;
