98f4c9ca599085d774c3cb7056a59cc3c99d4d1c
bongsub.kim <bongsub.kim@lgepartner.com>
2015-08-06 14:25:01 +0900

    ENYO-2153: Add user defined component to accessibilitySample
    https://jira2.lgsvl.com/browse/ENYO-2153
    Enyo-DCO-1.1-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    Change-Id: I1640f34cfba42fb8aa5ea779a9028a0dd4bb5677

--- src/moonstone-samples/lib/AccessibilitySample.js
+++ src/moonstone-samples/lib/AccessibilitySample.js
@@ -7,6 +7,7 @@ var
 
 var
 	BodyText = require('moonstone/BodyText'),
+	Checkbox = require('moonstone/Checkbox'),
 	Divider = require('moonstone/Divider'),
 	Icon = require('moonstone/Icon'),
 	Item = require('moonstone/Item'),
@@ -23,32 +24,46 @@ module.exports = kind({
 	labelText : 'Label',
 	hintText : 'Hint',
 	components: [
-		{kind: Header, name: 'header', content: 'Accessibility', type: 'small', components: [
+		{kind: Header, name: 'header', content: 'Accessibility', titleBelow: 'Unusual case sample for Accessibility', type: 'small', components: [
 			{name: 'labelButton', kind: Button, small: true, minWidth: false, content: 'Set Label', ontap: 'labelButtonTapped'},
 			{name: 'hintButton', kind: Button, small: true, minWidth: false, content: 'Set Hint', ontap: 'hintButtonTapped'},
-			{name: 'toggle', kind: ToggleButton, small: true, toggleOnLabel: 'disabled is on', toggleOffLabel: 'disabled is off', ontap: 'disabledTapped'},
-			{name: 'reload', kind: Button, small: true, content: 'Reload', ontap: 'reload'}
+			{name: 'toggle', kind: ToggleButton, small: true, toggleOnLabel: 'disabled is on', toggleOffLabel: 'disabled is off', ontap: 'disabledTapped'}
 		]},
 		{name: 'container', kind: Scroller, fit: true, components: [
 			{classes: 'moon-1v'},
-			{kind: Divider, content: 'Icons: '},
-			{kind: Icon, icon: 'play', small: false, ontap: 'buttonTapped', accessibilityLabel: 'play'},
-			{kind: Icon, src: '@../assets/icon-list.png', ontap: 'buttonTapped', accessibilityLabel: 'icon List'},
-			{kind: Item, classes: 'moon-hspacing', ontap: 'buttonTapped', accessibilityLabel: 'selectable icon List', components: [
-				{content: 'Selectable Item', accessibilityDisabled: true},
+			{kind: Control, tag: 'br'},
+			{kind: Divider, content: 'Case 1: Non spotlight'},
+			{kind: Icon, icon: 'play', small: false, ontap: 'buttonTapped'},
+			{kind: Icon, src: '@../assets/icon-list.png', ontap: 'buttonTapped'},
+			{classes: 'moon-1v'},
+			{kind: Control, tag: 'br'},
+			{kind: Divider, content: 'Case 2: User defined'},
+			{kind: Item, classes: 'moon-hspacing', ontap: 'buttonTapped', components: [
+				{content: 'Content and Icon'},
 				{kind: Icon, src: '@../assets/icon-list.png', ontap: 'buttonTapped'}
 			]},
-			{kind: Control, tag: 'br'},
+			{name: 'checkItem', kind: Item, classes: 'moon-hspacing', ontap: 'buttonTapped', components: [
+				{content: 'Content and Checkbox'},
+				{name: 'checkbox', kind: Checkbox, spotlight:false, checked: true}
+			]},
 			{classes: 'moon-1v'},
-			{kind: Divider, content: 'Icon Buttons: '},
-			{kind: IconButton, icon: 'drawer', small: false, ontap: 'buttonTapped', accessibilityLabel: 'drawer'},
-			{kind: IconButton, src: '@../assets/icon-list.png', small: false, ontap: 'buttonTapped', accessibilityLabel: 'icon List'}
+			{kind: Control, tag: 'br'},
+			{kind: Divider, content: 'Case 3: Non content'},
+			{kind: IconButton, icon: 'drawer', small: false, ontap: 'buttonTapped'},
+			{kind: IconButton, src: '@../assets/icon-list.png', small: false, ontap: 'buttonTapped'}
 		]},
 		{kind: Divider, content: 'Result'},
 		{kind: BodyText, name: 'console', content: 'No changes yet'}
 	],
+	rendered: function () {
+		Control.prototype.rendered.apply(this, arguments);
+		this.$.checkItem.setAttribute('role', 'checkbox');
+		this.$.checkItem.setAttribute('aria-checked', this.$.checkbox.getChecked());
+	},
 	buttonTapped: function (sender, event) {
 		this.$.console.setContent(sender.get('accessibilityDisabled') ? 'accessibility disabled' : sender.getAttribute('aria-label') +' tapped.');
+		this.$.checkbox.setChecked(!this.$.checkbox.getChecked());
+		this.$.checkItem.setAttribute('aria-checked', this.$.checkbox.getChecked());
 	},
 	labelButtonTapped: function (sender, event) {
 		this.$.header.setTitleBelow('Set all control\'s accessibilityLabel to \'Label\'');
@@ -56,6 +71,9 @@ module.exports = kind({
 			control = Object.keys(this.$);
 		for (i = 0; i < control.length; ++i) {
 			this.$[control[i]].set('accessibilityLabel', this.labelText);
+			if (this.$[control[i]] == this.$.checkItem) {
+				this.$[control[i]].setAttribute('role', 'checkbox');
+			}
 		}
 	},
 	hintButtonTapped: function (sender, event) {
@@ -64,6 +82,9 @@ module.exports = kind({
 			control = Object.keys(this.$);
 		for (i = 0; i < control.length; ++i) {
 			this.$[control[i]].set('accessibilityHint', this.hintText);
+			if (this.$[control[i]] == this.$.checkItem) {
+				this.$[control[i]].setAttribute('role', 'checkbox');
+			}
 		}
 	},
 	disabledTapped: function (sender, event) {
@@ -72,9 +93,9 @@ module.exports = kind({
 			control = Object.keys(this.$);
 		for (i = 0; i < control.length; ++i) {
 			this.$[control[i]].set('accessibilityDisabled', sender.value ? true : false);
+			if (this.$[control[i]] == this.$.checkItem) {
+				this.$[control[i]].setAttribute('role', 'checkbox');
+			}
 		}
-	},
-	reload: function (sender, event) {
-		window.location.reload();
 	}
 });
