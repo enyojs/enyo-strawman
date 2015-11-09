5b7b31e5db068461d0b6c81bc81e8d7b1eaf3f99
SUNG, Yunbum <yb.sung@lge.com>
2015-11-09 10:32:31 +0900

    Converted " to '
    Enyo-DCO-1.1-Signed-off-by: YB Sung <yb.sung@lge.com>

--- garnet/src/PickerPanelSample.js
+++ garnet/src/PickerPanelSample.js
@@ -10,23 +10,23 @@ var
 	Popup = require('garnet/Popup');
 
 var SamplePickerPanel = kind({
-	name: "g.sample.PickerPanel",
+	name: 'g.sample.PickerPanel',
 	kind: Panel,
 	events: {
-		onResult: ""
+		onResult: ''
 	},
 	components: [
-		{style: "position: relative; background-color: #000000;", classes: "g-common-width-height-fit", components: [
-			{name: "pickerButton", kind: FormPickerButton, style: "top: " + ri.scale(134) + "px;", ontap: "showPopup", content: "Click here!"},
-			{name: "pickerPopup", kind: Popup, effect: "depth-transition", components: [
-				{style: "background-color: #000000;position: relative;", classes: "g-common-width-height-fit g-layout-absolute-wrapper", components: [
-					{name: "pickerPanel", kind: GarnetPickerPanel, title: true, titleContent: "PickerTitle", ontap: "tapItem"}
+		{style: 'position: relative; background-color: #000000;', classes: 'g-common-width-height-fit', components: [
+			{name: 'pickerButton', kind: FormPickerButton, style: 'top: ' + ri.scale(134) + 'px;', ontap: 'showPopup', content: 'Click here!'},
+			{name: 'pickerPopup', kind: Popup, effect: 'depth-transition', components: [
+				{style: 'background-color: #000000;position: relative;', classes: 'g-common-width-height-fit g-layout-absolute-wrapper', components: [
+					{name: 'pickerPanel', kind: GarnetPickerPanel, title: true, titleContent: 'PickerTitle', ontap: 'tapItem'}
 				]}
 			]}
 		]}
 	],
 	bindings: [
-		{from: ".collection", to: ".$.pickerPanel.collection"}
+		{from: '.collection', to: '.$.pickerPanel.collection'}
 	],
 	create: kind.inherit(function(sup) {
 		return function() {
@@ -37,51 +37,51 @@ var SamplePickerPanel = kind({
 	rendered: function() {
 		// default index
 		this.$.pickerPanel.setIndex(2);
-		this.doResult({msg: "The item index #" + this.$.pickerPanel.getIndex() + " is selected."});
+		this.doResult({msg: 'The item index #' + this.$.pickerPanel.getIndex() + ' is selected.'});
 	},
 	showPopup: function(inSender, inEvent) {
-		if (inSender.name === "pickerButton") {
+		if (inSender.name === 'pickerButton') {
 			this.$.pickerPopup.show();
 		}
 	},
 	tapItem: function(inSender, inEvent) {
 		var index = this.$.pickerPanel.getIndex();
 
-		this.doResult({msg: "The item index #" + index + " is selected."});
+		this.doResult({msg: 'The item index #' + index + ' is selected.'});
 		this.$.pickerPopup.hide();
 		this.$.pickerButton.setContent((this.data[index]).item);
 	},
 	data: [
-		{item: "This item title is very long"},
-		{item: "Marquez"},
-		{item: "Barr"},
-		{item: "Everett"},
-		{item: "Crane"},
-		{item: "Raymond"},
-		{item: "Petersen"},
-		{item: "Kristina"},
-		{item: "Barbra"},
-		{item: "Tracey"},
-		{item: "Alejandra"},
-		{item: "Marquez"},
-		{item: "Barr"},
-		{item: "Everett"},
-		{item: "Crane"}
+		{item: 'This item title is very long'},
+		{item: 'Marquez'},
+		{item: 'Barr'},
+		{item: 'Everett'},
+		{item: 'Crane'},
+		{item: 'Raymond'},
+		{item: 'Petersen'},
+		{item: 'Kristina'},
+		{item: 'Barbra'},
+		{item: 'Tracey'},
+		{item: 'Alejandra'},
+		{item: 'Marquez'},
+		{item: 'Barr'},
+		{item: 'Everett'},
+		{item: 'Crane'}
 	]
 });
 
 module.exports = kind({
-	name: "g.sample.PickerPanelSample",
-	classes: "enyo-unselectable garnet g-sample",
+	name: 'g.sample.PickerPanelSample',
+	classes: 'enyo-unselectable garnet g-sample',
 	components: [
-		{content: "< PickerPanel Sample", classes: "g-sample-header", ontap: "goBack"},
+		{content: '< PickerPanel Sample', classes: 'g-sample-header', ontap: 'goBack'},
 
-		{content: "PickerPanel", classes: "g-sample-subheader"},
-		{kind: SamplePickerPanel, style: "position: relative;", onResult: "result"},
+		{content: 'PickerPanel', classes: 'g-sample-subheader'},
+		{kind: SamplePickerPanel, style: 'position: relative;', onResult: 'result'},
 
-		{src: "@../assets/btn_command_next.svg", classes: "g-sample-result", components: [
-			{content: "Result", classes: "g-sample-subheader"},
-			{name: "result", allowHtml: true, content: "No button pressed yet.", classes: "g-sample-description"}
+		{src: '@../assets/btn_command_next.svg', classes: 'g-sample-result', components: [
+			{content: 'Result', classes: 'g-sample-subheader'},
+			{name: 'result', allowHtml: true, content: 'No button pressed yet.', classes: 'g-sample-description'}
 		]}
 	],
 	result: function(inSender, inEvent) {
