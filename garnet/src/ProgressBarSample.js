ea7faba9d0989e8bb69df67d3aa18a2dc82ec4bf
hakhyun.jee <hakhyun.jee@lgepartner.com>
2015-11-18 19:29:24 +0900

    Remove components related garnet/Popup
    We have developed new panel components to replace components related
    popup for UX 1.1.
    So we removed components related garnet/Popup, and
    we need to modify samples.
    
    Checklist :
    
    sample-JSHint : No error ( Using gulp )
    
    Enyo-DCO-1.1-Signed-off-by: hakhyun.jee hakhyun.jee@lgepartner.com

--- garnet/src/ProgressBarSample.js
+++ garnet/src/ProgressBarSample.js
@@ -2,39 +2,18 @@ require('garnet');
 
 var
 	kind = require('enyo/kind'),
-	ConfirmPopup = require('garnet/ConfirmPopup'),
 	FormButton = require('garnet/FormButton'),
 	FormInput = require('garnet/FormInput'),
 	FormLabel = require('garnet/FormLabel'),
 	FormToolDecorator = require('garnet/FormToolDecorator'),
-	IconButton = require('garnet/IconButton'),
 	Panel = require('garnet/Panel'),
 	ProgressBar = require('garnet/ProgressBar');
 
 var ProgressBarPanel = kind({
 	name: 'g.sample.ProgressBarPanel',
 	kind: Panel,
-	handlers: {
-		onCancel: 'hidePopup',
-		onPopUpAnimationEnd: 'popupAnimationFinished'
-	},
 	components: [
 		{classes: 'g-sample-progressbar-header'},
-		{name: 'popupButton', kind: FormButton, ontap: 'showPopup', content: 'Popup', classes: 'g-sample-progressbar-popup-button'},
-		{
-			name: 'confirmPopupWithOnlyCancelButton',
-			kind: ConfirmPopup,
-			buttonComponents: [
-				{name: 'cancel', kind: IconButton, ontap: 'hidePopup', classes: 'g-cancel-image'}
-			],
-			components: [
-				{classes: 'g-sample-progressbar-popup g-common-width-height-fit', components: [
-					{name: 'percentage', classes:'g-sample-progressbar-popup-loading', content: 'Loading...'},
-					{kind: ProgressBar, name: 'popupProgress', classes:'g-sample-progressbar-popup-progressbar', progress: 0, max: 100, onChange: 'updateProgressP'},
-					{name: 'percentageP', classes:'g-sample-progressbar-popup-text'}
-				]}
-			]
-		},
 		{kind: FormLabel, content: 'Progress Bar: set value '},
 		{name: 'progressBar1', kind: ProgressBar, progress: 25, max: 100, classes: 'g-sample-progressbar-progressbar1'},
 		{name: 'progressBar2', kind: ProgressBar, progress: 25, bgProgress: 75, max: 100, classes: 'g-sample-progressbar-progressbar2'},
@@ -59,27 +38,6 @@ var ProgressBarPanel = kind({
 	decValue: function() {
 		this.$.input.setValue(Math.max(parseInt(this.$.input.getValue() || 0, 10) - 10, 0));
 		this.changeValue();
-	},
-	updateProgressP: function(inSender, inEvent) {
-		var value = Math.round(this.$.popupProgress.getProgress());
-		this.$.percentageP.setContent(value + ' %');
-		return false;
-	},
-	popupAnimationFinished: function(inSender, inEvent) {
-		if (inEvent.originator.name === 'confirmPopupWithOnlyCancelButton') {
-			this.$.popupProgress.animateProgressTo(100);
-		}
-	},
-	showPopup: function(inSender, inEvent) {
-		if (inSender.name === 'popupButton') {
-			this.$.confirmPopupWithOnlyCancelButton.show();
-		}
-	},
-	hidePopup: function(inSender, inEvent) {
-		if (inEvent.originator.name === 'cancel') {
-			this.$.confirmPopupWithOnlyCancelButton.hide();
-			this.$.popupProgress.setProgress(0);
-		}
 	},
 	goBack: function(inSender, inEvent) {
 		global.history.go(-1);
