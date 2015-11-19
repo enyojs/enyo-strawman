c148a05e12526271b5b73ae3fd100a8072ffa836
Mikyung Kim <mikyung27.kim@lge.com>
2015-11-19 16:45:14 +0900

    PickerPanelSample: inital try
    Enyo-DCO-1.1-Signed-off-by: Mikyung Kim <mikyung27.kim@lge.com>

--- garnet/src/PickerPanelSample.js
+++ garnet/src/PickerPanelSample.js
@@ -8,11 +8,52 @@ var
 	PickerPanel = require('garnet/PickerPanel'),
 	PanelManager = require('garnet/PanelManager');
 
-var Formatter = kind.singleton({
-	/*
-	* From PickerPanel.value to FormPickerButton.content
-	*/
-	PickerPanel: function(val, data) {
+
+var SamplePickerPanel = kind({
+	name: 'g.sample.PickerPanel',
+	kind: PickerPanel,
+	events: {
+		onUpdate: ''
+	},
+	title: true,
+	titleContent: 'PickerTitle',
+	create: kind.inherit(function(sup) {
+		return function() {
+			sup.apply(this, arguments);
+			this.collection = new Collection(data);
+		};
+	}),
+	deactivate: function(inEvent) {
+		if (!inEvent.dragging) {
+			this.doUpdate({name: this.name, value: this.value});
+		}
+	}
+});
+
+var FormPanel = kind({
+	name: 'g.sample.FormPanel',
+	kind: Panel,
+	events: {
+		onResult: ''
+	},
+	components: [
+		{name: 'pickerButton', kind: FormPickerButton, classes: 'g-sample-picker-panel-button', ontap: 'showPanel', content: 'Click here!'}
+	],
+	initComponents: kind.inherit(function(sup) {
+		return function() {
+			sup.apply(this, arguments);
+			this.$.pickerButton.setContent(data[2].item);
+		};
+	}),
+	showPanel: function(inSender, inEvent) {
+		this.container.pushFloatingPanel({name: 'pickerPanel', kind: PickerPanel, title: true, titleContent: 'PickerTitle', owner: this, onUpdate: 'updateContent'});
+	},
+	updateContent: function(inSender, inEvent) {
+		var content = this.formattingContent(inEvent.value);
+		this.$.pickerButton.setContent(content);
+		this.doResult({msg: content});
+	},
+	formattingContent: function(val, data) {
 		var
 			item = val,
 			name = 'No item';
@@ -28,75 +69,6 @@ var Formatter = kind.singleton({
 	}
 });
 
-var SamplePickerPanel = kind({
-	name: 'g.sample.PickerPanel',
-	kind: PickerPanel,
-	title: true,
-	titleContent: 'PickerTitle',
-	create: kind.inherit(function(sup) {
-		return function() {
-			sup.apply(this, arguments);
-			this.collection = new Collection(data);
-		};
-	}),
-	valueChanged: kind.inherit(function(sup) {
-		return function() {
-			sup.apply(this, arguments);
-			if (this.fromPanel) {
-				this.fromPanel.triggerHandler('onUpdate', {
-					name: this.name,
-					value: this.value
-				});
-			}
-		};
-	})
-});
-
-var FormPanel = kind({
-	name: 'g.sample.FormPanel',
-	kind: Panel,
-	events: {
-		onResult: ''
-	},
-	handlers: {
-		onUpdate: 'updateContent'
-	},
-	components: [
-		{name: 'pickerButton', kind: FormPickerButton, classes: 'g-sample-picker-panel-button', ontap: 'showPanel', content: 'Click here!'}
-	],
-	initComponents: kind.inherit(function(sup) {
-		return function() {
-			sup.apply(this, arguments);
-			this.$.pickerButton.setContent(data[2].item);
-		};
-	}),
-	showPanel: function(inSender, inEvent) {
-		this.bubbleUp('onPushPanel', {panel: {name: 'pickerPanel', kind: SamplePickerPanel, owner: this, fromPanel: this, selectedIndex: 2}});
-	},
-	updateContent: function(inSender, inEvent) {
-		var content = Formatter.PickerPanel(inEvent.value);
-		this.$.pickerButton.setContent(content);
-		this.doResult({msg: content});
-	}
-});
-
-var PanelManager = kind({
-	kind: PanelManager,
-	handlers: {
-		onPushPanel: 'pushPanel',
-		onPopPanel: 'popPanel'
-	},
-	components: [
-		{kind: FormPanel, classes: 'g-sample-panel', onResult: 'result'}
-	],
-	pushPanel: function (inSender, inEvent) {
-		this.pushFloatingPanel(inEvent.panel, inEvent.options);
-	},
-	popPanel: function (inSender, inEvent) {
-		this.popFloatingPanel();
-	}
-});
-
 module.exports = kind({
 	name: 'g.sample.PickerPanelSample',
 	classes: 'enyo-unselectable garnet g-sample',
@@ -104,8 +76,9 @@ module.exports = kind({
 		{content: '< PickerPanel Sample', classes: 'g-sample-header', ontap: 'goBack'},
 
 		{content: 'PickerPanel', classes: 'g-sample-subheader'},
-		{kind: PanelManager, classes: 'g-sample-panel-manager'},
-
+		{kind: PanelManager, classes: 'g-sample-panel-manager', components: [
+			{kind: FormPanel, classes: 'g-sample-panel', onResult: 'result'}
+		]},
 		{src: '@../assets/btn_command_next.svg', classes: 'g-sample-result', components: [
 			{content: 'Result', classes: 'g-sample-subheader'},
 			{name: 'result', allowHtml: true, content: 'No button pressed yet.', classes: 'g-sample-description'}
