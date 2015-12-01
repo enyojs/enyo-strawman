9788a57610bd5b0705ba1bc07b3a1e6cd22fabf8
Stephen Choi <stephen.choi@lge.com>
2015-12-01 15:08:07 -0800

    ENYO-2885: Float left for rtl locale Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- src/moonstone-samples/lib/TooltipSample.js
+++ src/moonstone-samples/lib/TooltipSample.js
@@ -24,7 +24,7 @@ module.exports = kind({
 				{kind: Tooltip, content: 'I\'m a left tooltip.', position: 'above'}
 			]},
 
-			{kind: TooltipDecorator, style: 'float:right', components: [
+			{kind: TooltipDecorator, classes: 'right', components: [
 				{kind: Button, content: 'Right Tooltip'},
 				{name: 'toolTip', kind: Tooltip, uppercase: false, content: 'I\'m a right tooltip.'}
 			]}
@@ -39,7 +39,7 @@ module.exports = kind({
 				]}
 			]},
 
-			{kind: TooltipDecorator, style: 'float: right', components: [
+			{kind: TooltipDecorator, classes: 'right', components: [
 				{kind: Button, small: true, content: 'Multiline Right'},
 				{kind: Tooltip, position: 'above', components: [
 					{content: 'I\'m a right tooltip that is rather long and forces a line break.', style: 'width: 300px; white-space: normal'}
@@ -53,7 +53,7 @@ module.exports = kind({
 				{kind: Tooltip, floating: true, content: 'I\'m a left floating tooltip.'}
 			]},
 
-			{kind: TooltipDecorator, style: 'float: right', components: [
+			{kind: TooltipDecorator, classes: 'right', components: [
 				{kind: Button, disabled: true, small: true, content: 'Item with Right Floating Tooltip'},
 				{name: 'toolTipFloating', floating: true, kind: Tooltip, position: 'above', content: 'I\'m a right floating tooltip.'}
 			]}
@@ -67,7 +67,7 @@ module.exports = kind({
 				{kind: Tooltip, floating: true, content: 'I\'m a tooltip for an input.', position: 'above'}
 			]},
 
-			{kind: TooltipDecorator, style: 'float:right;', components: [
+			{kind: TooltipDecorator, classes: 'right', components: [
 				{kind: InputDecorator, components: [
 					{kind: Input, style: 'width: 130px;', placeholder: 'Below'}
 				]},
@@ -81,7 +81,7 @@ module.exports = kind({
 				{kind: Tooltip, floating: true, content: 'Floating tooltip for an IconButton.'}
 			]},
 
-			{kind: TooltipDecorator, style: 'float:right;', components: [
+			{kind: TooltipDecorator, classes: 'right', components: [
 				{kind: IconButton, src: '@../assets/icon-button-enyo-logo.png'},
 				{kind: Tooltip, floating: false, content: 'I\'m a tooltip for an IconButton.'}
 			]}
