eac2d26cc113eefeacf816bbd7032c4ff6503909
Stephen Choi <stephen.choi@lge.com>
2015-12-01 16:16:12 -0800

    Add draggable tooltip for better testing Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- src/moonstone-samples/lib/TooltipSample.js
+++ src/moonstone-samples/lib/TooltipSample.js
@@ -1,5 +1,6 @@
 var
-	kind = require('enyo/kind');
+	kind = require('enyo/kind'),
+	dom = require('enyo/dom');
 
 var
 	FittableRows = require('layout/FittableRows');
@@ -14,77 +15,98 @@ var
 
 module.exports = kind({
 	name: 'moon.sample.TooltipSample',
-	kind: FittableRows,
 	classes: 'moon enyo-unselectable enyo-fit',
 	components: [
-		//Top row of buttons
-		{classes: 'moon-5v', components:[
-			{kind: TooltipDecorator, components: [
-				{kind: Button, disabled: true, centered: true, content: 'Left Tooltip'},
-				{kind: Tooltip, content: 'I\'m a left tooltip.', position: 'above'}
-			]},
+		{name: 'dragContainer', kind: TooltipDecorator, classes: 'draggable',
+			ondragstart: 'dragstart', ondrag: 'drag', ondragfinish: 'dragfinish', components: [
+				{name: 'dragBtn', kind: Button, content: 'Draggble Tooltip'},
+				{kind: Tooltip, content: 'I\'m a Draggble tooltip.', position: 'above'}
+			]
+		},
+		{kind: FittableRows, components: [
+			//Top row of buttons
+			{classes: 'moon-5v', components:[
+				{kind: TooltipDecorator, components: [
+					{kind: Button, disabled: true, centered: true, content: 'Left Tooltip'},
+					{kind: Tooltip, content: 'I\'m a left tooltip.', position: 'above'}
+				]},
 
-			{kind: TooltipDecorator, classes: 'right', components: [
-				{kind: Button, content: 'Right Tooltip'},
-				{name: 'toolTip', kind: Tooltip, uppercase: false, content: 'I\'m a right tooltip.'}
-			]}
-		]},
-		//Second row of buttons
-		{classes: 'moon-5v', components:[
-			{kind: TooltipDecorator, components: [
-				{kind: Button, small: true, content: 'Multiline Left Tooltip'},
-				{kind: Tooltip, components: [
-					{content: 'I\'m a left tooltip.'},
-					{content: 'With a second line of content'}
+				{kind: TooltipDecorator, classes: 'right', components: [
+					{kind: Button, content: 'Right Tooltip'},
+					{name: 'toolTip', kind: Tooltip, uppercase: false, content: 'I\'m a right tooltip.'}
 				]}
 			]},
+			//Second row of buttons
+			{classes: 'moon-5v', components:[
+				{kind: TooltipDecorator, components: [
+					{kind: Button, small: true, content: 'Multiline Left Tooltip'},
+					{kind: Tooltip, components: [
+						{content: 'I\'m a left tooltip.'},
+						{content: 'With a second line of content'}
+					]}
+				]},
 
-			{kind: TooltipDecorator, classes: 'right', components: [
-				{kind: Button, small: true, content: 'Multiline Right'},
-				{kind: Tooltip, position: 'above', components: [
-					{content: 'I\'m a right tooltip that is rather long and forces a line break.', style: 'width: 300px; white-space: normal'}
+				{kind: TooltipDecorator, classes: 'right', components: [
+					{kind: Button, small: true, content: 'Multiline Right'},
+					{kind: Tooltip, position: 'above', components: [
+						{content: 'I\'m a right tooltip that is rather long and forces a line break.', style: 'width: 300px; white-space: normal'}
+					]}
 				]}
-			]}
-		]},
-		//Third row of buttons
-		{classes: 'moon-5v', components:[
-			{kind: TooltipDecorator, components: [
-				{kind: Button, small: true, content: 'Item with Left Floating Tooltip'},
-				{kind: Tooltip, floating: true, content: 'I\'m a left floating tooltip.'}
 			]},
-
-			{kind: TooltipDecorator, classes: 'right', components: [
-				{kind: Button, disabled: true, small: true, content: 'Item with Right Floating Tooltip'},
-				{name: 'toolTipFloating', floating: true, kind: Tooltip, position: 'above', content: 'I\'m a right floating tooltip.'}
-			]}
-		]},
-		//Fourth row of buttons
-		{fit: true, components:[
-			{kind: TooltipDecorator, components: [
-				{kind: InputDecorator, components: [
-					{kind: Input, style: 'width: 130px;', placeholder: 'Above'}
+			//Third row of buttons
+			{classes: 'moon-5v', components:[
+				{kind: TooltipDecorator, components: [
+					{kind: Button, small: true, content: 'Item with Left Floating Tooltip'},
+					{kind: Tooltip, floating: true, content: 'I\'m a left floating tooltip.'}
 				]},
-				{kind: Tooltip, floating: true, content: 'I\'m a tooltip for an input.', position: 'above'}
-			]},
 
-			{kind: TooltipDecorator, classes: 'right', components: [
-				{kind: InputDecorator, components: [
-					{kind: Input, style: 'width: 130px;', placeholder: 'Below'}
+				{kind: TooltipDecorator, classes: 'right', components: [
+					{kind: Button, disabled: true, small: true, content: 'Item with Right Floating Tooltip'},
+					{name: 'toolTipFloating', floating: true, kind: Tooltip, position: 'above', content: 'I\'m a right floating tooltip.'}
+				]}
+			]},
+			//Fourth row of buttons
+			{fit: true, components:[
+				{kind: TooltipDecorator, components: [
+					{kind: InputDecorator, components: [
+						{kind: Input, style: 'width: 130px;', placeholder: 'Above'}
+					]},
+					{kind: Tooltip, floating: true, content: 'I\'m a tooltip for an input.', position: 'above'}
 				]},
-				{kind: Tooltip, content: 'I\'m a tooltip for an input.', position: 'below'}
-			]}
-		]},
-		//Bottom row of buttons
-		{components:[
-			{kind: TooltipDecorator, components: [
-				{kind: IconButton, src: '@../assets/icon-button-enyo-logo.png'},
-				{kind: Tooltip, floating: true, content: 'Floating tooltip for an IconButton.'}
-			]},
 
-			{kind: TooltipDecorator, classes: 'right', components: [
-				{kind: IconButton, src: '@../assets/icon-button-enyo-logo.png'},
-				{kind: Tooltip, floating: false, content: 'I\'m a tooltip for an IconButton.'}
+				{kind: TooltipDecorator, classes: 'right', components: [
+					{kind: InputDecorator, components: [
+						{kind: Input, style: 'width: 130px;', placeholder: 'Below'}
+					]},
+					{kind: Tooltip, content: 'I\'m a tooltip for an input.', position: 'below'}
+				]}
+			]},
+			//Bottom row of buttons
+			{components:[
+				{kind: TooltipDecorator, components: [
+					{kind: IconButton, src: '@../assets/icon-button-enyo-logo.png'},
+					{kind: Tooltip, floating: true, content: 'Floating tooltip for an IconButton.'}
+				]},
+
+				{kind: TooltipDecorator, classes: 'right', components: [
+					{kind: IconButton, src: '@../assets/icon-button-enyo-logo.png'},
+					{kind: Tooltip, floating: false, content: 'I\'m a tooltip for an IconButton.'}
+				]}
 			]}
 		]}
-	]
+	],
+	dragstart: function() {
+		this.$.dragBtn.spotlight = false;
+	},
+	drag: function(sender, ev) {
+		var x = parseInt(this.rtl? ev.clientX - 1920 + 162 : ev.clientX - 162, 10),
+			y = parseInt(ev.clientY - 42, 10);
+
+
+		dom.transform(this.$.dragContainer, {translateX: x + 'px'});
+		dom.transform(this.$.dragContainer, {translateY: y + 'px'});
+	},
+	dragfinish: function() {
+		this.$.dragBtn.spotlight = true;
+	}
 });
