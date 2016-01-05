8c93a94d9264b44edae87b0d76fd5011d116b6bb
Aaron Tam <aaron.tam@lge.com>
2016-01-05 15:02:20 -0800

    ENYO-2214: Utilize new request syntax.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- index.js
+++ index.js
@@ -7,28 +7,20 @@ var
 	strawman = require('./src');
 
 ready(function () {
-	
+
 	var names = window.document.location.search.substring(1).split('&'),
 		name = names[0],
 		Sample = strawman.samples[name] || strawman.List;
-	
-	if(Sample instanceof window.request) {
-	
+
+	if (request.isRequest(Sample)) {
 		//check to see if the sample is wrapped in
 		//request instead of being required into the build
-		
-		//do something with moonstone history now
-		strawman.History.then(function(history){
-			history.set('enableBackHistoryAPI', false);
-		});
-		
+
 		Sample.then(function(res){
 			new res().renderInto(document.body);
 		});
-		
-		
 	} else {
-		
+
 		new Sample().renderInto(document.body);
 	}
 });
