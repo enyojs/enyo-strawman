56606a48468b175288cd4c36142963b0d382e9d1
Aaron Tam <aaron.tam@lge.com>
2016-01-05 15:09:40 -0800

    ENYO-2214: Lazily load moonstone-extra samples.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- moonstone-extra/index.js
+++ moonstone-extra/index.js
@@ -7,12 +7,13 @@ var
 var
 	SampleList = require('../src/strawman/SampleList');
 
-require('../src/moonstone-samples');	// Included for its assets and CSS
+var
+	resources = request('../src/moonstone-samples');	// Included for its assets and CSS
 
 var
 	samples = {
 		Enyo: require('../src/enyo-samples'),
-		Moonstone: require('./src'), //router blocking
+		Moonstone: request('./src'), //router blocking
 		Layout: require('../src/layout-samples'),
 		Spotlight: require('../src/spotlight-samples'),
 		iLib: require('../src/enyo-ilib-samples'),
@@ -30,11 +31,31 @@ var
 		samples: samples
 	});
 
+function renderLazySample (sample) {
+	sample.then(function (res) {
+		new res().renderInto(document.body);
+	});
+}
+
 ready(function () {
 	var names = window.document.location.search.substring(1).split('&'),
 		name = names[0],
 		Sample = samples[name] || List;
-	new Sample().renderInto(document.body);
+
+	if (request.isRequest(Sample)) {
+		//check to see if the sample is wrapped in
+		//request instead of being required into the build
+
+		if (name == 'Moonstone') {
+			resources.then(function () { // load Moonstone resources, first
+				renderLazySample(Sample);
+			});
+		} else {
+			renderAsyncSample(Sample);
+		}
+	} else {
+		new Sample().renderInto(document.body);
+	}
 });
 
 module.exports = {
