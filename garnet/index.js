af6dd2118b63973963e3611acb76a2709a84b538
Jason Robitaille <jason.robitaille@lge.com>
2016-01-21 16:03:03 -0800

    ENYO-2921: PlatformSample(W2+): Unable to Navigate Back to Sample List from PlatformSample ENYO-2920: PlatformSample(W2+): Unable to Scroll Sample to View ENYO-2897: PlatformSample (W2+): Text Needs Visible Color For a Black Background

--- garnet/index.js
+++ garnet/index.js
@@ -2,6 +2,7 @@ require('enyo/options').accessibility = true;
 
 var
 	ready = require('enyo/ready'),
+	Scroller = require('garnet/Scroller'),
 	kind = require('enyo/kind');
 
 var
@@ -30,8 +31,26 @@ var
 
 ready(function () {
 	var names = window.document.location.search.substring(1).split('&'),
-		name = names[0],
+		name = names[0];
+	
+	if(samples[name] && names.length===2 && name !== "Garnet") {
+		var Sample = kind({
+			classes:'enyo-fit g-sample',
+			style:'background:white;',
+			components: [
+				{content: '<', classes: 'g-sample-header g-back-enyo', ontap: 'goBack'},
+				{kind:Scroller, style:"width:100%; height:100%", components:[
+					{kind:samples[name], style:"padding-bottom:60px;"},
+				]}
+			],
+			goBack: function(inSender, inEvent) {
+				global.history.go(-1);
+				return false;
+			}
+		});
+	} else {
 		Sample = samples[name] || List;
+	}
 	new Sample().renderInto(document.body);
 });
 
