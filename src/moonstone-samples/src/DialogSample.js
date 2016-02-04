40761c80efe32ce47251f44d07f82142c7993cbd
Blake Stephens <blake.stephens@lge.com>
2016-02-04 10:38:26 +0900

    PLAT-6842: Added new features for example and testing purposes. Also updated binding style.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- src/moonstone-samples/src/DialogSample.js
+++ src/moonstone-samples/src/DialogSample.js
@@ -5,6 +5,8 @@ var
 	Divider = require('moonstone/Divider'),
 	Button = require('moonstone/Button'),
 	CheckboxItem = require('moonstone/CheckboxItem'),
+	InputDecorator = require('moonstone/InputDecorator'),
+	Input = require('moonstone/Input'),
 	Dialog = require('moonstone/Dialog');
 
 module.exports = kind({
@@ -21,12 +23,24 @@ module.exports = kind({
 		{kind: CheckboxItem, content: 'Animate', name: 'animateToggle'},
 		{kind: CheckboxItem, content: 'Lock 5-way inside popup (spotlightModal)', name: 'spotlightModalToggle'},
 		{kind: CheckboxItem, content: 'Close by back key (allowBackKey)', name: 'allowBackKeyToggle'},
+		{kind: CheckboxItem, content: 'Use divider to separate title from body', name: 'useDivider'},
+		{classes: 'moon-1v'},
+		{kind: Divider, content: 'Set a custom title, or remove the titles'},
+		{kind: InputDecorator, components: [
+			{kind: Input, placeholder: 'Title', value: 'Dialog Title', name: 'inputTitle'}
+		]},
+		{kind: InputDecorator, components: [
+			{kind: Input, placeholder: 'Sub-Title', value: 'A subtitle for dialog.', name: 'inputSubTitle'}
+		]},
 		{
 			name: 'dialog',
 			kind: Dialog,
 			title: 'You\'ve been watching TV for a very long time so let\'s do a quick check-in.',
 			subTitle: 'This TV has been active for 10 hours.',
 			message: 'Perhaps it is time to take a break and get some fresh air. There is a nice coffee shop around the corner',
+			// message: [
+			// 	{kind: Button, content: 'Fancy button'}
+			// ],
 			components: [
 				{kind: Button, content: 'Go get a coffee', ontap: 'hideDialog'},
 				{kind: Button, content: 'Keep watching TV', ontap: 'addMessage'}
@@ -34,12 +48,15 @@ module.exports = kind({
 		}
 	],
 	bindings: [
-		{from: '.$.dialog.allowBackKey', to: '.$.allowBackKeyToggle.checked', oneWay: false},
-		{from: '.$.dialog.modal', to: '.$.modalToggle.checked', oneWay: false},
-		{from: '.$.dialog.animate', to: '.$.animateToggle.checked', oneWay: false},
-		{from: '.$.dialog.autoDismiss', to: '.$.autoDismissToggle.checked', oneWay: false},
-		{from: '.$.dialog.spotlightModal', to: '.$.spotlightModalToggle.checked', oneWay: false},
-		{from: '.$.dialog.showCloseButton', to: '.$.showCloseButtonToggle.checked', oneWay: false}
+		{from: '$.dialog.autoDismiss', to: '$.autoDismissToggle.checked', oneWay: false},
+		{from: '$.dialog.modal', to: '$.modalToggle.checked', oneWay: false},
+		{from: '$.dialog.showCloseButton', to: '$.showCloseButtonToggle.checked', oneWay: false},
+		{from: '$.dialog.animate', to: '$.animateToggle.checked', oneWay: false},
+		{from: '$.dialog.spotlightModal', to: '$.spotlightModalToggle.checked', oneWay: false},
+		{from: '$.dialog.allowBackKey', to: '$.allowBackKeyToggle.checked', oneWay: false},
+		{from: '$.dialog.useDivider', to: '$.useDivider.checked', oneWay: false},
+		{from: '$.dialog.title', to: '$.inputTitle.value', oneWay: false},
+		{from: '$.dialog.subTitle', to: '$.inputSubTitle.value', oneWay: false}
 	],
 	showDialog: function (sender) {
 		this.$.dialog.show();
