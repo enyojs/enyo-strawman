b7960ef5d2c471a8401f30a73e821b2061e3bb1e
Blake Stephens <blake.stephens@lge.com>
2016-04-25 17:44:07 -0700

    Refactored the sample to work and look better. It's now updated to the current Notification component structure.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- src/moonstone-samples/src/NotificationSample.js
+++ src/moonstone-samples/src/NotificationSample.js
@@ -13,35 +13,49 @@ module.exports = kind({
 	name: 'moon.sample.NotificationSample',
 	classes: 'moon enyo-unselectable enyo-fit moon-notification-sample',
 	components: [
-		{kind: Divider, content: 'Notifications'},
+		{classes: 'button-container', components: [
+			{kind: Divider, content: 'Small Notifications'},
+			{classes: 'moon-hspacing moon-vspacing-s', components: [
+				{kind: Button, content: 'Say Hello', ontap: 'showPopup', popup: 'greetingNotification'},
+				{kind: Button, content: 'Basic', ontap: 'showPopup', popup: 'basicNotification'}
+			]},
+			{classes: 'moon-1v'},
+			{kind: Divider, content: 'Large Notifications'},
+			{classes: 'moon-hspacing moon-vspacing-s', components: [
+				{kind: Button, content: 'Many buttons', ontap: 'showPopup', popup: 'componentNotification'},
+				{kind: Button, content: 'No buttons', ontap: 'showPopup', popup: 'emptyNotification'},
+				{kind: Button, content: 'Hide', ontap: 'hidePopup', popup: 'emptyNotification'}
+			]},
+			{classes: 'moon-1v'},
+			{kind: Divider, content: 'Immediete Notifications'},
+			{classes: 'moon-hspacing moon-vspacing-s', components: [
+				{kind: Button, content: 'Show', ontap: 'showPopup', popup: 'basicNotification', direct: true},
+				{kind: Button, content: 'Hide', ontap: 'hidePopup', popup: 'basicNotification', direct: true}
+			]},
+			{classes: 'moon-1v'},
+			{kind: Divider, content: 'Customize Notifications\' Contents'},
+			{kind: InputDecorator, spotlight: true, components: [
+				{name: 'contentsField', kind: Input, placeholder: 'Notification Contents', value: 'I\'m a notification...'}
+			]},
+		]},
 
-		{classes: 'moon-hspacing moon-vspacing-s', components: [
-			{kind: Button, content: 'Plain Text', ontap: 'showPopup', popup: 'basicNotification'},
-			{kind: Button, content: 'Hide', ontap: 'hidePopup', popup: 'basicNotification'}
+		{name: 'greetingNotification', kind: Notify, content: 'Hello', components: [
+			{kind: Button, content: 'Howdy', small: true, ontap: 'hidePopup', popup: 'greetingNotification'}
 		]},
-		{classes: 'moon-1v'},
-		{classes: 'moon-hspacing moon-vspacing-s', components: [
-			{kind: Button, content: 'Components', ontap: 'showPopup', popup: 'componentNotification'}
+		{name: 'basicNotification', kind: Notify, content: 'Small notification', components: [
+			{kind: Button, content: 'Close', small: true, ontap: 'hidePopup', popup: 'basicNotification'}
 		]},
-		{classes: 'moon-1v'},
-		{classes: 'moon-hspacing moon-vspacing-s', components: [
-			{kind: Button, content: 'Non-Animating', ontap: 'showPopup', popup: 'basicNotification', direct: true},
-			{kind: Button, content: 'Hide', ontap: 'hidePopup', popup: 'basicNotification', direct: true}
-		]},
-		{classes: 'moon-1v'},
-		{kind: Divider, content: 'Customize Notifications\' Contents'},
-		{kind: InputDecorator, spotlight: true, components: [
-			{name: 'contentsField', kind: Input, placeholder: 'Notification Contents', value: 'I\'m a notification...'}
-		]},
-
-		{name: 'basicNotification', kind: Notify},
-		{name: 'componentNotification', kind: Notify, classes: 'moon-hspacing', components: [
-			{name: 'bodyTextContent', kind: BodyText, styles: 'white-space: wrap'},
+		{name: 'emptyNotification', kind: Notify, content: 'Not even any buttons'},
+		{name: 'componentNotification', kind: Notify, content: 'Not to worry, this message isn\'t going to be very long. It just has to be long enough to show what a long message looks like. That\'s all; have a nice day.' , components: [
+			{kind: Button, content: 'First Button!', small: true},
+			{kind: Button, content: 'Kittens', small: true},
+			{kind: Button, content: 'Oh my yes, kittens', small: true},
 			{kind: Button, content: 'Close', small: true, ontap: 'hidePopup', popup: 'componentNotification'}
 		]}
 	],
 	bindings: [
 		{from: '$.contentsField.value', to: '$.basicNotification.content'},
+		{from: '$.contentsField.value', to: '$.emptyNotification.content'},
 		{from: '$.contentsField.value', to: '$.bodyTextContent.content'}
 	],
 	showPopup: function (sender) {
