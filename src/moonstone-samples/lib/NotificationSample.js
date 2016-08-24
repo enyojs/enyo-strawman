bf19f8780907ffebaeea57f571d45c5c85d3f357
Kunmyon Choi <kunmyon.choi@lge.com>
2016-08-24 15:49:27 +0900

    Add tooltip button on notification case.

--- src/moonstone-samples/lib/NotificationSample.js
+++ src/moonstone-samples/lib/NotificationSample.js
@@ -6,6 +6,8 @@ var
 	Button = require('moonstone/Button'),
 	Input = require('moonstone/Input'),
 	InputDecorator = require('moonstone/InputDecorator'),
+	Tooltip = require('moonstone/Tooltip'),
+	TooltipDecorator = require('moonstone/TooltipDecorator'),
 	Divider = require('moonstone/Divider');
 
 module.exports = kind({
@@ -49,7 +51,10 @@ module.exports = kind({
 		{name: 'componentNotification', kind: Notify, wide: true, content: 'Not to worry, this message isn\'t going to be very long. It just has to be long enough to show what a long message looks like. That\'s all; have a nice day.' , components: [
 			{kind: Button, content: 'First Button!'},
 			{kind: Button, content: 'Oh my yes, kittens'},
-			{kind: Button, content: 'hide and show', ontap: 'hideshow'},
+			{kind: TooltipDecorator, classes: 'right', components: [
+				{kind: Button, content: 'hide and show', ontap: 'hideshow'},
+				{name: 'toolTip', kind: Tooltip, uppercase: false, content: 'To show tooltip case.'}
+			]},
 			{kind: Button, content: 'Close', ontap: 'hidePopup', popup: 'componentNotification'}
 		]},
 		{name: 'animateNotification', kind: Notify, animate: true, content: 'Animate notification', components: [
