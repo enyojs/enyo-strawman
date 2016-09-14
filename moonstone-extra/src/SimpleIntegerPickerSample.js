27873ea78d7583e33fccaf683e52cedb2be7454b
Blake Stephens <blake.stephens@lge.com>
2016-09-14 09:37:11 -0700

    Adjusted position of the ranges in the sample.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- moonstone-extra/src/SimpleIntegerPickerSample.js
+++ moonstone-extra/src/SimpleIntegerPickerSample.js
@@ -13,16 +13,19 @@ module.exports = kind({
 		{kind: Divider, content: 'SimpleIntegerPicker'},
 		{kind: BodyText, content: 'Clicking the Enter/Ok button or directly the number keys on the component will enable the "Input field" of the component.<br />After the value is inputted, confirm the number by pressing Enter/Ok button.'},
 		{components: [
-			{content: 'Audio offset (-10 to +10):', tag: 'span'},
-			{kind: SimpleIntegerPicker, min: -10, value: -4, max: 10, onChange: 'change'}
+			{content: 'Audio offset:', tag: 'label'},
+			{kind: SimpleIntegerPicker, min: -10, value: -4, max: 10, onChange: 'change'},
+			{kind: BodyText, content: '(-10 to +10)', tag: 'span'}
 		]},
 		{components: [
-			{content: 'Brightnesss (1 to 100):', tag: 'label'},
-			{kind: SimpleIntegerPicker, min: 1, value: 50, max: 100, unit: 'lumens', onChange: 'change'}
+			{content: 'Brightnesss:', tag: 'label'},
+			{kind: SimpleIntegerPicker, min: 1, value: 50, max: 100, unit: 'lumens', onChange: 'change'},
+			{kind: BodyText, content: '(1 to 100)', tag: 'span'}
 		]},
 		{components: [
-			{content: 'Volume (1 to 100):', tag: 'label'},
-			{kind: SimpleIntegerPicker, value: 45, min: 1, max: 100, unit: '', onChange: 'change'}
+			{content: 'Volume:', tag: 'label'},
+			{kind: SimpleIntegerPicker, value: 45, min: 1, max: 100, unit: '', onChange: 'change'},
+			{kind: BodyText, content: '(1 to 100)', tag: 'span'}
 		]},
 		{classes: 'moon-3v'},
 		{kind: Divider, content: 'Result'},
